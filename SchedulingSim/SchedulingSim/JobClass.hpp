90ce7fad35d804182c1abbc4668bf2aff85cc18a
Unknown <d00169143@student.dkit.ie>
2018-03-15 03:20:01 +0000

    Round Robin working in job class, and printing all schdules in a table

--- SchedulingSim/SchedulingSim/JobClass.hpp
+++ SchedulingSim/SchedulingSim/JobClass.hpp
@@ -15,6 +15,14 @@ struct Jobs
 	}
 };
 
+struct scheduling
+{
+	std::string FIFO;
+	std::string SJF;
+	std::string STTC;
+	std::string RR;
+};
+
 class JobClass
 {
 	public:
@@ -31,15 +39,22 @@ class JobClass
 		void							FIFO(std::list<Jobs> joblist, int tick);
 		void							STTC(std::list<Jobs> joblist, int tick);
 		void							SJF(std::list<Jobs> joblist, int tick);
-
+		void							RR(std::list<Jobs> joblist, int tick);
+		void							prinSchedule();
 
 		
 	private:
 		//allowing myself to add 5 Jobs to the struct
 		Jobs							JOB[5];
+		scheduling						schedules[100];
+
 		std::list<Jobs>					initial_list;
 
-		std::list<Jobs>					sjf_list;
+		std::list<scheduling>			scheduling;
+		std::list<std::string>			fifo_list;
+		std::list<std::string>			sjf_list;
+		std::list<std::string>			stcf_list;
+		std::list<std::string>			rr_list;
 
 		//integrating time
 		int								TICK;
